dfad2b697291db369706c5df3f7c580815cb2c5d
sxuns <865567694@qq.com>
2018-11-08 10:15:52 +0800

    静态页面第一次提交

---
+++ webpack/test02/src/main.js
@@ -0,0 +1,22 @@
+let {add} = require('./index');
+let Vue = require('../vue.min');
+let VM = new Vue({
+    el:"#app",
+    data:{
+        name:"计算：",
+        number1:0,
+        number2:0,
+        result:0,
+        list:[1,2,3,4,5],
+        user:{userName:"tom",age:13},
+        userList:[
+            {user:{userName:"Jack",age:11}},
+            {user:{userName:"John",age:12}}
+        ]
+    },
+    methods:{
+        change:function () {
+            this.result = add(parseInt(this.number1),parseInt(this.number2));
+        }
+    }
+});
